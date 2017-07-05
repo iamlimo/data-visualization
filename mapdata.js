4d685615a20d176cac7bc2ad149141270a43d5a8
iamlimo <isaac.orija@gmail.com>
2017-07-05 13:26:53 +0100

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,320 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+    width: "500", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "green",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: "12",
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "http://simplemaps.com/static/img/basketballs.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    location_image_url: ""
+  },
+  state_specific: {
+    NGA2839: {
+      hide: "default",
+      description: "Population: 1282829292",
+      color: "#d40e0e",
+      url: "default",
+      inactive: "default",
+      zoomable: "default",
+      hover_color: "default",
+      name: "Borno"
+    },
+    NGA2841: {
+      color: "#d40e0e",
+      name: "Abia",
+      description: "Population: 1282829292"
+    },
+    NGA2842: {
+      color: "#f02525",
+      name: "Akwa Ibom",
+      description: "Population: 1282829292"
+    },
+    NGA2843: {
+      color: "#f02525",
+      name: "Imo",
+      description: "Population: 1282829292"
+    },
+    NGA2844: {
+      color: "#740808",
+      name: "Rivers",
+      description: "Population: 1282829292"
+    },
+    NGA2845: {
+      color: "#f02525",
+      name: "Bayelsa",
+      description: "Population: 1282829292"
+    },
+    NGA2846: {
+      color: "#f02525",
+      name: "Benue",
+      description: "Population: 1282829292"
+    },
+    NGA2847: {
+      color: "#f02525",
+      name: "Cross River",
+      description: "Population: 1282829292"
+    },
+    NGA2848: {
+      color: "#d40e0e",
+      name: "Taraba",
+      description: "Population: 1282829292"
+    },
+    NGA2849: {
+      color: "#d40e0e",
+      name: "Kwara",
+      description: "Population: 1282829292"
+    },
+    NGA2850: {
+      color: "#f02525",
+      name: "Lagos",
+      description: "Population: 1282829292"
+    },
+    NGA2851: {
+      color: "#740808",
+      name: "Niger",
+      description: "Population: 1282829292"
+    },
+    NGA2852: {
+      color: "#740808",
+      name: "Ogun",
+      description: "Population: 1282829292"
+    },
+    NGA2853: {
+      color: "#f02525",
+      name: "Ondo",
+      description: "Population: 1282829292"
+    },
+    NGA2854: {
+      color: "#740808",
+      name: "Ekiti",
+      description: "Population: 1282829292"
+    },
+    NGA2855: {
+      color: "#440505",
+      name: "Osun",
+      description: "Population: 1282829292"
+    },
+    NGA2856: {
+      color: "#f02525",
+      name: "Oyo",
+      description: "Population: 1282829292"
+    },
+    NGA2857: {
+      color: "#440505",
+      name: "Anambra",
+      description: "Population: 1282829292"
+    },
+    NGA2858: {
+      color: "#740808",
+      name: "Bauchi",
+      description: "Population: 1282829292"
+    },
+    NGA2859: {
+      color: "#d40e0e",
+      name: "Gombe",
+      description: "Population: 1282829292"
+    },
+    NGA2860: {
+      color: "#440505",
+      name: "Delta",
+      description: "Population: 1282829292"
+    },
+    NGA2861: {
+      color: "#440505",
+      name: "Edo",
+      description: "Population: 1282829292"
+    },
+    NGA2862: {
+      color: "#440505",
+      name: "Enugu",
+      description: "Population: 1282829292"
+    },
+    NGA2863: {
+      color: "#440505",
+      name: "Ebonyi",
+      description: "Population: 1282829292"
+    },
+    NGA2864: {
+      color: "#740808",
+      name: "Kaduna",
+      description: "Population: 1282829292"
+    },
+    NGA2865: {
+      color: "#440505",
+      name: "Kogi",
+      description: "Population: 1282829292"
+    },
+    NGA2866: {
+      color: "#440505",
+      name: "Plateau",
+      description: "Population: 1282829292"
+    },
+    NGA2867: {
+      color: "#740808",
+      name: "Nassarawa",
+      description: "Population: 1282829292"
+    },
+    NGA2868: {
+      color: "#740808",
+      name: "Jigawa",
+      description: "Population: 1282829292"
+    },
+    NGA2869: {
+      color: "#740808",
+      name: "Kano",
+      description: "Population: 1282829292"
+    },
+    NGA2870: {
+      color: "#740808",
+      name: "Katsina",
+      description: "Population: 1282829292"
+    },
+    NGA2871: {
+      color: "#740808",
+      name: "Sokoto",
+      description: "Population: 1282829292"
+    },
+    NGA2872: {
+      color: "#740808",
+      name: "Zamfara",
+      description: "Population: 1282829292"
+    },
+    NGA2873: {
+      color: "#740808",
+      name: "Yobe",
+      description: "Population: 1282829292"
+    },
+    NGA2879: {
+      color: "#740808",
+      name: "Kebbi",
+      description: "Population: 1282829292"
+    },
+    NGA2881: {
+      color: "#740808",
+      name: "Adamawa",
+      description: "Population: 1282829292"
+    },
+    NGA3470: {
+      color: "#740808",
+      name: "Federal Capital Territory",
+      description: "Population: 1282829292"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "9.083333",
+      lng: "7.533333",
+      name: "Abuja",
+      color: "default",
+      description: "default",
+      url: "default",
+      size: "default",
+      type: "default",
+      image_url: "http://simplemaps.com/static/img/basketball.png",
+      opacity: "default"
+    }
+  },
+  regions: {},
+  data: {
+    data: {
+      NGA2839: "1000000",
+      NGA2841: "222222",
+      NGA2842: "22222",
+      NGA2843: "22222",
+      NGA2844: "12346473",
+      NGA2845: "12344",
+      NGA2846: "12344",
+      NGA2847: "22222",
+      NGA2848: "1000000",
+      NGA2849: "1000000",
+      NGA2850: "1233",
+      NGA2851: "12346473",
+      NGA2853: "112122",
+      NGA2852: "2332322",
+      NGA2855: "24242484",
+      NGA2860: "24242484",
+      NGA2859: "123344",
+      NGA2856: "2321",
+      NGA2854: "13233243",
+      NGA2861: "190000000000",
+      NGA2862: "1213323213",
+      NGA2863: "32323232",
+      NGA2864: "2121313",
+      NGA2865: "39391891",
+      NGA2866: "3131339303",
+      NGA2867: "2121313",
+      NGA2868: "2121313",
+      NGA2869: "2121313",
+      NGA2871: "2121313",
+      NGA2870: "2121313",
+      NGA2872: "2121313",
+      NGA2873: "2121313",
+      NGA2879: "2121313",
+      NGA2881: "2121313",
+      NGA3470: "2121313",
+      NGA2858: "2828292",
+      NGA2857: "2223929903"
+    }
+  }
+};
